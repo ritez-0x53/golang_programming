02591a814a93cf9377f991d1f71c03f57f686964
ritez-0x53 <riteswarsrgy@gmail.com>
2025-09-15 10:50:04 +0530

    completed function, next starting variadic function

--- 12.function/main.go
+++ 12.function/main.go
@@ -10,11 +10,38 @@ func getLangs() (string, string, string) {
 	return "c", "go", "js"
 }
 
+func logName(name string) {
+	fmt.Println(name)
+}
+
+// * passing function as a parameter like in JS - callback
+func processIt(fn func(a int) int, val int) {
+	result := fn(val)
+	fmt.Println(result)
+}
+
+func myFunc() func(a int) int {
+	return func (a int) int  {
+		return a
+	}
+} 
+
+
 func main() {
 	res := add(2, 5)
 	fmt.Println(res)
 
 	lang1, lang2, lang3 := getLangs()
 	fmt.Println(lang1, lang2, lang3)
+	logName("riteswar")
+
+	fn := func(val int) int {
+		return val
+	}
+	processIt(fn, 6000)
+
+	run := myFunc()
+	res1 := run(200)
+	fmt.Println(res1)
 
 }
